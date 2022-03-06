b3b3c32a2fb1f8d98efafe68492f1c148704e9c4
NotEnoughMilk <jaredomano@gmail.com>
2022-03-06 00:20:33 -0500

    \/

--- public_html/assets/js/main.js
+++ public_html/assets/js/main.js
@@ -5,18 +5,19 @@
 * License: https://bootstrapmade.com/license/
 */
 
-function getAge(dateString) {
-  var today = new Date();
-  var birthDate = new Date(dateString);
-  var age = today.getFullYear() - birthDate.getFullYear();
-  var m = today.getMonth() - birthDate.getMonth();
-  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-      age--;
-  }
-  return age;
-}
-
 (function() {
+
+  function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+  }
+
   "use strict";
 
   /**
