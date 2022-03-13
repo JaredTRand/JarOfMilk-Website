a529c1f9b3a60168fc8c76a947fd1a1828603591
NotEnoughMilk <jaredomano@gmail.com>
2022-03-12 21:21:27 -0500

    ok

--- public_html/namegen/assets/js/main.js
+++ public_html/namegen/assets/js/main.js
@@ -17,30 +17,6 @@ function randColor(){
       dataType: 'text',
       success: function(data) { 
         $('#generatedName').text(data);
-
-        var end = Date.now() + (150);
-        var colors = [randColor(), randColor()];
-        (function frame() {
-          confetti({
-            particleCount: 2,
-            angle: 60,
-            spread: 55,
-            origin: { x: 0 },
-            colors: colors
-          });
-          confetti({
-            particleCount: 2,
-            angle: 120,
-            spread: 55,
-            origin: { x: 1 },
-            colors: colors
-          });
-          
-          if (Date.now() < end) {
-            requestAnimationFrame(frame);
-          }
-        }());
-        frame();
       }
     });
   }); 
